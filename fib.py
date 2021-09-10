8711b6b087faa2aed0555e93418f695ad4e6872e
raggs1561 <72584468+raggs1561@users.noreply.github.com>
2021-09-10 12:53:40 -0400

    Added test cases
    Test cases cover Fibonacci numbers 1 through 8

--- fib.py
+++ fib.py
@@ -21,6 +21,30 @@ print("The 0th Fibonacci number: ", fibonacci(0)) # should return 0
 assert(fibonacci(-1) == None)
 print("The -1st Fibonacci number: ", fibonacci(-1)) # should return None
 
+assert(fibonacci(1) == 1)
+print("The 1st Fibonacci number: ", fibonacci(1)) # should return 1
+
+assert(fibonacci(2) == 1)
+print("The 2nd Fibonacci number: ", fibonacci(2)) # should return 1
+
+assert(fibonacci(3) == 2)
+print("The 3rd Fibonacci number: ", fibonacci(3)) # should return 2
+
+assert(fibonacci(4) == 3)
+print("The 4th Fibonacci number: ", fibonacci(4)) # should return 3
+
+assert(fibonacci(5) == 5)
+print("The 5th Fibonacci number: ", fibonacci(5)) # should return 5
+
+assert(fibonacci(6) == 8)
+print("The 6th Fibonacci number: ", fibonacci(6)) # should return 8
+
+assert(fibonacci(7) == 13)
+print("The 7th Fibonacci number: ", fibonacci(7)) # should return 13
+
+assert(fibonacci(8) == 21)
+print("The 8th Fibonacci number: ", fibonacci(8)) # should return 21
+
 print("Code ran successfully!")
 
 
